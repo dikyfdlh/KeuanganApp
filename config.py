93567172250b442e971f9448a55d9bfbcbbe4725
Diky Fadhilah <dikyfdlh77@gmail.com>
2025-05-15 00:40:14 +0700

    Fix: config.py

--- config.py
+++ config.py
@@ -4,5 +4,5 @@ BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = 'rahasia'
-    SQLALCHEMY_DATABASE_URI = 'mysql://root:pAPqAfzaZAzKvQBUJnCXjAgPgQNimRaF@hopper.proxy.rlwy.net:55845/railway'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:pAPqAfzaZAzKvQBUJnCXjAgPgQNimRaF@hopper.proxy.rlwy.net:55845/railway'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
